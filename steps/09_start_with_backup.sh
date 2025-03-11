be8457e817ef41bf85a4de7ce9688c7dd881fcd1
Irina Truong <i.chernyavska@gmail.com>
2025-03-11 10:56:46 -0700

    Comment.

--- steps/09_start_with_backup.sh
+++ steps/09_start_with_backup.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/env bash
-# sudo vim /usr/local/var/postgresql@17/postgresql.conf
-# replace data_directory
+# sudo vim
+#    /usr/local/var/postgresql@17/postgresql.conf
+#    /opt/homebrew/var/postgresql@17/postgresql.conf
+# depending on brew --prefix
+# replace data_directory with /Users/jbennet/src/postgres-backup-demo/combined-backup
 
 brew services stop postgresql@17
 chmod 700 ./combined-backup
+# chown -R postgres:postgres ./combined-backup
 brew services start postgresql@17
